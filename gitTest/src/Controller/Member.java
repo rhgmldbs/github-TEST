467dd566461a59e17fb881154da565c05d5e83e8
user <user@user-PC>
2019-12-19 11:06:45 +0900

    2019.12.19

---
+++ gitTest/src/Controller/Member.java
@@ -0,0 +1,8 @@
+package Controller;
+
+public class Member {
+
+	public void doTest() {
+		System.out.println("####여기는 doTest.java 이다.");
+	}
+}
---
+++ gitTest/src/dao/MemberDAO.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class MemberDAO {
+
+	public void getDB() {
+		System.out.println("###여기는 DAO함수###");
+	}
+}
